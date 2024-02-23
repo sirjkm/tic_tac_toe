862fdc973dc881cb95b8d326d9f103108c2b7d00
Jason Manning <jasonkylemanning@gmail.com>
2024-02-23 13:10:27 -0500

    update with AI

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import time
-from player import HumanPlayer, RandomComputerPlayer
+from player import HumanPlayer, RandomComputerPlayer, GeniusComputerPlayer
 
 class TicTacToe:
     def __init__(self):
@@ -84,7 +84,19 @@ def play(game, x_player, o_player, print_game=True):
         print('It\'s a tie.')
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = RandomComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(100):
+        x_player = RandomComputerPlayer('X')
+        o_player = GeniusComputerPlayer('O')
+        t = TicTacToe()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+
+    print('After 100 iterations, we see {x_wins} X wins, {o_wins} O wins, and {ties} ties.')
